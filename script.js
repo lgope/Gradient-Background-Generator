d3177c4abc41eb592005989ae454f51f6c014a83
lgope <ilakshmangope@gmail.com>
2020-01-11 23:53:55 +0600

    error fixed! 🗽🛠

--- script.js
+++ script.js
@@ -5,26 +5,22 @@ let body = document.getElementById('gradient');
 
 let randomBtn = document.getElementById('random-color-btn');
 
-function setGradient() {
+const setGradient = () => {
   body.style.background = `linear-gradient(to right, ${color1.value} , ${color2.value})`;
 
   css.textContent = `${body.style.background};`;
-}
+};
 
-function rc() {
-  var r1 = '#' + ((Math.random() * 0xffffff) << 0).toString(16);
-  return r1;
-}
-function randomColor() {
-  color1.value = rc();
-  color2.value = rc();
-  setgrad();
-}
-console.log(css);
-console.log(color1.value);
-console.log(color2.value);
+const randomColorGernator = () =>
+  `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
 
-rand.addEventListener('click', randomColor);
+const randomColor = () => {
+  color1.value = randomColorGernator();
+  color2.value = randomColorGernator();
+  setGradient();
+};
+
+randomBtn.addEventListener('click', randomColor);
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
